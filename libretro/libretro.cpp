560bc33e72a593b076b2f12aa47fbc22c9470416
iLag <ilagdoesntcheckthisemail@gmail.com>
2017-03-29 10:09:43 -0700

    Add support for multiline cheats.

--- libretro/libretro.cpp
+++ libretro/libretro.cpp
@@ -371,28 +371,36 @@ void retro_cheat_reset()
    S9xApplyCheats();
 }
 
-void retro_cheat_set(unsigned index, bool enabled, const char *code)
+void retro_cheat_set(unsigned index, bool enabled, const char *codeline)
 {
    uint32 address;
    uint8 val;
 
    bool8 sram;
    uint8 bytes[3];//used only by GoldFinger, ignored for now
+   char codeCopy[256];
+   char* code;
 
-   if (S9xGameGenieToRaw(code, address, val)!=NULL &&
-       S9xProActionReplayToRaw(code, address, val)!=NULL &&
-       S9xGoldFingerToRaw(code, address, sram, val, bytes)!=NULL)
-   { // bad code, ignore
-      return;
+   strcpy(codeCopy,codeline);
+   code=strtok(codeCopy,"+,.; ");
+   while (code != NULL) {
+      if (S9xGameGenieToRaw(code, address, val)!=NULL &&
+          S9xProActionReplayToRaw(code, address, val)!=NULL &&
+          S9xGoldFingerToRaw(code, address, sram, val, bytes)!=NULL)
+      { // bad code, ignore
+         return;
+      }
+      if (index>Cheat.num_cheats) return; // cheat added in weird order, ignore
+      if (index==Cheat.num_cheats) Cheat.num_cheats++;
+
+      Cheat.c[index].address = address;
+      Cheat.c[index].byte = val;
+      Cheat.c[index].enabled = enabled;
+
+      Cheat.c[index].saved = FALSE; // it'll be saved next time cheats run anyways
+
+      code=strtok(codeCopy,"+,.; ");
    }
-   if (index>Cheat.num_cheats) return; // cheat added in weird order, ignore
-   if (index==Cheat.num_cheats) Cheat.num_cheats++;
-
-   Cheat.c[index].address = address;
-   Cheat.c[index].byte = val;
-   Cheat.c[index].enabled = enabled;
-
-   Cheat.c[index].saved = FALSE; // it'll be saved next time cheats run anyways
 
    Settings.ApplyCheats=true;
    S9xApplyCheats();
