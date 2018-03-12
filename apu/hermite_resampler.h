12b16b681a5101590bf49b03c2c50ef2a2a079e1
Dwedit <danweiss@gmail.com>
2018-03-12 16:20:55 -0500

    Fixes audio samples in the output buffer being destroyed after saving and loading state.

--- apu/hermite_resampler.h
+++ apu/hermite_resampler.h
@@ -66,6 +66,13 @@ class HermiteResampler : public Resampler
         void
         read (short *data, int num_samples)
         {
+			//If we are outputting the exact same ratio as the input, pull directly from the input buffer
+			if (r_step == 1.0)
+			{
+				ring_buffer::pull((unsigned char*)data, num_samples * sizeof(short));
+				return;
+			}
+
             int i_position = start >> 1;
             int max_samples = buffer_size >> 1;
             short *internal_buffer = (short *) buffer;
@@ -120,7 +127,13 @@ class HermiteResampler : public Resampler
         inline int
         avail (void)
         {
-            return (int) floor (((size >> 2) - r_frac) / r_step) * 2;
+			//If we are outputting the exact same ratio as the input, find out directly from the input buffer
+			if (r_step == 1.0)
+			{
+				return (ring_buffer::space_filled() + sizeof(short) - 1) / sizeof(short);
+			}
+
+			return (int) floor (((size >> 2) - r_frac) / r_step) * 2;
         }
 };
 
