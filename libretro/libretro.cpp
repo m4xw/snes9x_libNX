28fcb4d3cf2a5e726091f0d177c55c242c45d1d4
kps501 <39468366+kps501@users.noreply.github.com>
2018-06-05 16:01:40 +0000

    libretro: missing init descriptors

--- libretro/libretro.cpp
+++ libretro/libretro.cpp
@@ -177,7 +177,7 @@ void retro_set_environment(retro_environment_t cb)
    static const struct retro_controller_info ports[] = {
       { port_1, 3 },
       { port_2, 6 },
-      { 0 },
+      { 0, 0 },
    };
 
    environ_cb(RETRO_ENVIRONMENT_SET_CONTROLLER_INFO, (void*)ports);
@@ -667,7 +667,7 @@ static void init_descriptors(void)
       { 4, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_SELECT,   "Select" },
       { 4, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_START,    "Start" },
 
-      { 0 },
+      { 0, 0, 0, 0, "" },
    };
 
    environ_cb(RETRO_ENVIRONMENT_SET_INPUT_DESCRIPTORS, desc);
